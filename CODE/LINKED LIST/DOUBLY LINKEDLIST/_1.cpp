a187b5ef0995d3da4f8567be632044f53202572e
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2023-08-21 01:16:20 +0530

    LINKEDLIST

--- CODE/LINKED LIST/DOUBLY LINKEDLIST/_1.cpp
+++ CODE/LINKED LIST/DOUBLY LINKEDLIST/_1.cpp
@@ -4,56 +4,54 @@ using namespace std;
 class Node{
     public:
     int data;
-    Node* prev;
     Node* next;
+    Node* prev;
     Node(){
         this->data=0;
-        this->prev=NULL;
         this->next=NULL;
+        this->prev=NULL;
     }
     Node(int data){
         this->data=data;
-        this->prev=NULL;
         this->next=NULL;
+        this->prev=NULL;
     }
 };
-void print(Node* &head){
-    Node* temp = head;
-    while(temp != NULL){
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
- 
-int getLength(Node* &head){
-    Node* temp = head;
-    int len =0;
-    while(temp != NULL){
-        temp = temp->next;
+int findlength(Node* &head){
+    int len=0;
+    Node* temp =head;
+    while (temp!=NULL)
+    {
+        temp=temp->next;
         len++;
     }
     return len;
 }
-void InsertatHead(Node* &head,int data){
-    Node* newNode = new Node(data);
-    newNode->next=head;
-    head->prev=newNode;
-    head=newNode;
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+    }
 }
-
+void insertathead(int data,Node* &head,Node* &tail){
+    if(head==NULL){
+        Node* temp=new Node(data);
+        head=temp;
+        tail=temp;
+        return;
+    } 
+    Node* temp;
+    temp->data=data;
+    temp->next=head;
+    head->prev=temp;
+    head=temp;
+}
 int main(){
-    Node* first = new Node(10);
-    Node* second = new Node(20);
-    Node* third = new Node(30);
-
     Node* head;
-    InsertatHead(head,100);
-    head->next=first;
-    first->prev=head;
-    first->next=second;
-    second->prev=first;
-    second->next=third;
-    third->prev= second;
+    Node* tail;
+    head->next=tail;
+    tail->prev=head;
+    insertathead(8,head,tail);
     print(head);
-     
 }
