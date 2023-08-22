10acc3e46b0fb269fb7154d830ae0e44f2bf68ba
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2023-08-22 14:05:21 +0530

    MODIFIED

---
+++ CODE/LINKED LIST/DOUBLY LINKEDLIST/_2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+    Node(){
+        this->data=0;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+    Node(int data){
+        this->data=data;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+};
+
+int main(){
+    
+}
