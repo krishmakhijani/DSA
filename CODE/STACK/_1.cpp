930572a02fe14a4dd59af610195d8adba0fa5b33
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2023-09-03 16:56:22 +0530

    STACK

--- CODE/STACK/_1.cpp
+++ CODE/STACK/_1.cpp
@@ -1,24 +1,6 @@
 #include<iostream>
-#include<stack>
 using namespace std;
 
 int main(){
-    stack<int> st;
-    st.push(21);
-    st.push(30);
-    st.push(40);
-    st.push(50);
-    st.push(70);
-    st.push(60);
-    st.push(32);
-    st.push(34);
-
-    st.pop();
-
-    cout << "Element on top " << st.top() << endl;
-    cout<< "Size : "<< st.size()<<endl;
-    if(st.empty())
-    cout<< "STACK IS EMPTY";
-    else
-    cout << "Not Empty";
+	cout << "HELLO" <<endl;
 }
