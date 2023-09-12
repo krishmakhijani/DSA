4dd7ee4dcc8f4a39d13d1f29e9a91cd02aaa40aa
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2023-09-12 09:36:39 +0530

    STACK LAB

--- CODE/LAB/STACK/_1.c
+++ CODE/LAB/STACK/_1.c
@@ -27,25 +27,41 @@ void display() {
     printf("%d ", stack[i]);
     printf("\n");
 }
+void peak(){
+    if(top == -1) {
+    printf("Stack is empty\n");
+    return;
+    }
+    printf("%d\n",stack[top]);
+}
 void main() {
     int ch, data;
     while(1) {
-    printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+    printf("1. Push\n2. Pop\n3. Display\n4. Peak\n5. Exit\n");
     printf("Enter your choice: ");
     scanf("%d", &ch);
     switch(ch) {
-    case 1: printf("Enter data: ");
+    case 1:
+    printf("Enter data to be inserted: ");
     scanf("%d", &data);
     push(data);
     break;
-    case 2: data = pop();
+    case 2:
+    data = pop();
     if(data != -1)
-    printf("Popped data is %d\n", data);
+    printf("Deleted element is %d\n", data);
     break;
-    case 3: display();
+    case 3:
+    display();
     break;
-    case 4: exit(0);
-    default: printf("Invalid choice\n");
+    case 4:
+    peak();
+    break;
+    case 5:
+    exit(0);
+    default:
+    printf("Invalid choice\n");
     }
     }
+    
 }
